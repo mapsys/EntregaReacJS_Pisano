d6793dbc063888faa73cdecf45f7d8562c3ce35a
Mariano Pisano <mapsyserick@hotmail.com>
2025-02-14 15:25:50 -0300

    Cambiando Layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,30 @@ import ItemListContainer from "./componentes/ItemListContainer";
 import ItemDetailContainer from "./componentes/ItemDetailContainer";
 import { BrowserRouter, Routes, Route } from "react-router";
 import NoEncontrada from "./componentes/NoEncontrada";
+import Header from "./componentes/Header";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<ItemListContainer />} />
-        <Route path="/category/:id" element={<ItemListContainer />} />
-        <Route path="/producto/:id" element={<ItemDetailContainer />} />
-        <Route path="*" element={ <NoEncontrada />} />
-      </Routes>
+      <div className="appContainer">
+        <div className="appHeader">
+          <Header></Header>
+        </div>
+        <div className="appCentralPage">
+          <div className="categoryBar">
+            <NavBar />
+          </div>
+          <div className="mainContent">
+            <Routes>
+              <Route path="/" element={<ItemListContainer />} />
+              <Route path="/category/:id" element={<ItemListContainer />} />
+              <Route path="/producto/:id" element={<ItemDetailContainer />} />
+              <Route path="*" element={<NoEncontrada />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
     </BrowserRouter>
   );
 }
--- src/componentes/NavBar.jsx
+++ src/componentes/NavBar.jsx
@@ -1,33 +1,36 @@
 import "../estilos/NavBar.css";
-import CartWidget from "../componentes/CartWidget";
+
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { getCategories } from "../constants/urls";
 import { Link } from "react-router";
-import logo from "../assets/pngegg.png"
+
 function NavBar() {
   const categorias = getCategories;
   return (
     <Navbar expand="lg" className="bg-body-tertiary mainHeader">
-      <Container>
-        <Navbar.Brand href="/" className="logo">
-          <img alt="logo" src={logo} className="d-inline-block align-top" />{" "}
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav menuItems">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            {categorias.map((categoria, index) => (
-              <Nav.Link key={index} as={Link} to={`/category/${categoria}`}>{categoria}</Nav.Link>
-            ))}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-      <CartWidget />
+      <div className="navBarContainer">
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav ">
+            <Nav className="me-auto">
+              <div className="menuItems">
+                <Nav.Link as={Link} to="/">
+                  Home
+                </Nav.Link>
+                {categorias.map((categoria, index) => (
+                  <Nav.Link key={index} as={Link} to={`/category/${categoria}`}>
+                    {categoria}
+                  </Nav.Link>
+                ))}
+              </div>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </div>
     </Navbar>
   );
 }
-
 
 export default NavBar;
